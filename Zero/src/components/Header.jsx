e9c109ccfd6b88df5c8c85d8fa35ccd0218d5e38
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-07-16 00:41:21 +0530

    done

--- Zero/src/components/Header.jsx
+++ Zero/src/components/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import logo from '../assets/logo.png'
 
 function Header() {
   return (
-    <header className="bg-zinc-800 p-4">
+    <header className="bg-black p-4 fixed w-full mt-3">
       <div className="container mx-auto flex justify-between items-center">
         {/* Logo */}
-        <div className="text-white font-bold text-xl">
+        {/* <div className="text-white font-bold text-xl ">
           Logo
+        </div> */}
+        <div>
+          <img src={logo} alt="logo" className="w-[50px] h-[50px]"/>
         </div>
 
         {/* Navigation Links */}
-        <nav className="flex-1 flex ml-[40vh]  justify-center space-x-4">
+        <nav className="flex-1 flex ml-[30vw]  justify-between" >
           <a href="#" className="text-white hover:text-gray-300">Home</a>
           <a href="#" className="text-white hover:text-gray-300">Shop</a>
           <a href="#" className="text-white hover:text-gray-300">About</a>
--- Zero/src/components/Home.jsx
+++ Zero/src/components/Home.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
+
 function Home() {
   return (
-    <div className="h-screen bg-zinc-800 flex items-center">
+    <div className="h-screen bg-black flex items-center text-white">
       <div className="w-[18vh] flex justify-center">
-        <div className="text-[20vh] font-bold transform -rotate-90 text-white font-lobster">
+        <div className="zero text-[25vh] border-b font-bold transform -rotate-90 text-white  ml-48 tracking-wider">
           ZERO
+        </div>
+        <div className=' text-white absolute right-32 top-40 box '>
+          <h1 className='text-3xl'>Clothes that <br /> Speak Volumes</h1>
+          <p className=' font-light pt-5'>Discover expressive fashion <br /> that speaks volumes, reflecting your unique <br /> style and personality effortlessly and elegantly.</p>
+
+          </div>
+
+        <div className="style absolute bottom-7 left-24 ">
+          Style Redefined
+        </div>
+
+        <div className=' absolute bottom-5 left-[45vw] flex align-bottom'>
+          <div className=' rotate-[270deg] mt-1 text-lg'>We have</div>
+          
+          <div className='-ml-10 font-light'>
+            {/* <p className=' border-b'>Style diversity</p> */}
+            <p className=' border-b'>Customer focus</p>
+            <p className=' border-b'>Quality Emphasis</p>
+            <p className=' border-b'>Exclusive Collections</p>
+          </div>
         </div>
       </div>
     </div>
