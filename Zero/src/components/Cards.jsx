ad579b0d7f6583a813f9572497e07fd60a826e4f
Neelesh <130912265+neeleshkr22@users.noreply.github.com>
2024-07-17 16:38:54 +0530

    u

--- Zero/src/components/Cards.jsx
+++ Zero/src/components/Cards.jsx
@@ -1,60 +1,39 @@
+
 import React, { useEffect } from 'react';
 import "../Cards.css";
 
 export default function Cards() {
   useEffect(() => {
     const card = document.querySelector('.cards');
-    const card1 = document.querySelector('.card1');
-    const card2 = document.querySelector('.card2');
-    const card3 = document.querySelector('.card3');
-
-    const handleMouseEnterCard = () => {
+    const card1 = document.querySelector('.card1')
+    const card2 = document.querySelector('.card2')
+    const card3 = document.querySelector('.card3')
+    card.addEventListener('click', function() {
       card1.style.transform = 'rotate(60deg)';
-      card1.style.marginTop = '250px';
-      card1.style.marginLeft = '350px';
+      card1.style.marginTop = '250px'
+      card1.style.marginLeft = '350px'
       card2.style.transform = 'rotate(-60deg)';
-      card2.style.marginTop = '220px';
-      card2.style.marginLeft = '-350px';
-    };
-
-    const handleMouseLeaveCard = () => {
-      card1.style.transform = 'rotate(0deg)';
-      card1.style.marginTop = '0px';
-      card1.style.marginLeft = '0px';
-      card2.style.transform = 'rotate(0deg)';
-      card2.style.marginTop = '0px';
-      card2.style.marginLeft = '0px';
-      card.style.transform = 'rotate(0deg)';
-      card.style.marginTop = '0px';
-      card.style.marginLeft = '0px';
-    };
-
-    card.addEventListener('mouseenter', handleMouseEnterCard);
-    card.addEventListener('mouseleave', handleMouseLeaveCard);
-
-    card1.addEventListener('mouseenter', function () {
-      card.style.transform = 'rotate(-60deg)';
-      card.style.marginTop = '350px';
-      card.style.marginLeft = '-500px';
+      card2.style.marginTop = '220px'
+      card2.style.marginLeft = '-350px'
     });
+    
+    card1.addEventListener('click' , function(){
+        card.style.transform = 'rotate(-60deg)';
+        card.style.marginTop = '350px'
+        card.style.marginLeft = '-500px'
 
-    card1.addEventListener('mouseleave', handleMouseLeaveCard);
+    })
 
-    card3.addEventListener('mouseenter', function () {
-      card.style.transform = 'rotate(0deg)';
-      card.style.marginTop = '0px';
-      card.style.marginLeft = '0px';
-    });
-
-    card3.addEventListener('mouseleave', handleMouseLeaveCard);
+    card3.addEventListener('click' , function(){
+        card.style.transform = 'rotate(0deg)';
+        card.style.marginTop = '00px'
+        card.style.marginLeft = '0px'
+    })
 
     return () => {
-      card.removeEventListener('mouseenter', handleMouseEnterCard);
-      card.removeEventListener('mouseleave', handleMouseLeaveCard);
-      card1.removeEventListener('mouseenter', handleMouseEnterCard);
-      card1.removeEventListener('mouseleave', handleMouseLeaveCard);
-      card3.removeEventListener('mouseenter', handleMouseEnterCard);
-      card3.removeEventListener('mouseleave', handleMouseLeaveCard);
+      card.removeEventListener('click', function() {
+        card1.style.transform = 'rotate(50deg)';
+      });
     };
   }, []);
 
