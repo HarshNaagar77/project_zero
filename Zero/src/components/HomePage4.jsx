3cd1e1a2cd041f0696c8a1512ac1014b66d4721e
Harsh Naagar <140304256+HarshNaagar77@users.noreply.github.com>
2024-08-01 17:06:07 +0530

    update

--- Zero/src/components/HomePage4.jsx
+++ Zero/src/components/HomePage4.jsx
@@ -8,17 +8,17 @@ function HomePage4() {
       <div className="relative flex-1 flex justify-center items-center">
         <img src={t1} alt="" className="relative mb-44 h-auto max-h-full transform -rotate-[30deg] right-36" />
         <h1 className="absolute top-1/4 mt-20 right-20 text-center font-extrabold text-6xl leading-tight transition-transform duration-300 ease-in-out">
-          <span className="text-white hover:scale-110 hover:underline">This is the</span> <span className="text-black hover:scale-110 hover:underline">new Gen</span>
+          <span className="text-white hover:scale-110 hover:underline">This is the</span> <span className="text-black hover:scale-110 hover:underline">new Gen</span> 
         </h1>
       </div>
       <div className="relative flex-1 flex justify-center items-center">
         <img src={t2} alt="" className="relative right-0 h-auto max-h-full" />
         <h1 className="absolute top-1/4 right-48 text-center font-extrabold text-5xl leading-tight transition-transform duration-300 ease-in-out">
-          <span className="text-black hover:scale-110 hover:underline">Experienc</span><span className="text-white hover:scale-110 hover:underline">e the Future Now</span>
+          <span className="text-black hover:scale-110 hover:underline">Experience</span><span className="text-white hover:scale-110 hover:underline">e the Future Now</span>
         </h1>
       </div>
     </div>
   );
-}
+} 
 
 export default HomePage4
